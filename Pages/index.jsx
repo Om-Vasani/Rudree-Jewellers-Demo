e3f28ec0b7044cb6d418d595c30e052ddfff622d
Om-Vasani <omvasani63@gmail.com>
2025-11-08 10:55:00 +0530

    Update index.jsx

--- Pages/index.jsx
+++ Pages/index.jsx
@@ -1,4 +1,4 @@
-    import Header from '../components/Header'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import ProductGrid from '../components/ProductGrid'
