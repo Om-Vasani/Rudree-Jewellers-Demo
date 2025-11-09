91ae7a6213fae4095f460ffec53cab0b02b25f47
Om-Vasani <omvasani63@gmail.com>
2025-11-09 11:30:28 +0530

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,15 +1,15 @@
 import Link from 'next/link'
-import Image from 'next/image'
 
 export default function Header() {
   return (
     <header className="shadow bg-white sticky top-0 z-50">
       <div className="container flex items-center justify-between py-4 mx-auto">
-        <Link href="/" className="flex items-center space-x-2"
+        <Link href="/" className="flex items-center space-x-2">
           <span className="text-xl font-semibold text-yellow-700">
             Rudree Jewellers
           </span>
         </Link>
+
         <nav className="space-x-6 text-gray-700">
           <Link href="/">Home</Link>
           <Link href="/products">Products</Link>
