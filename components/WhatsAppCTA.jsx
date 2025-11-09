319675aeea6cdba943be8b2644563e8406e7f6c6
Om-Vasani <omvasani63@gmail.com>
2025-11-09 12:00:31 +0530

    Update WhatsAppCTA.jsx

--- components/WhatsAppCTA.jsx
+++ components/WhatsAppCTA.jsx
@@ -5,7 +5,7 @@ export default function WhatsAppCTA() {
     <a
       href="https://wa.me/919999999999"
       target="_blank"
-      className="fixed bottom-[60px] right-5 z-50 hover:scale-110 transition-transform"
+      className="fixed bottom-[90px] right-5 z-50 hover:scale-110 transition-transform"
     >
       <Image
         src="/images/whatsapp.png"
